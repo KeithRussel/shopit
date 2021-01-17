a5688c7cf0cf587f44b82bc5968bf9ad4ad9070c
KeithRussel <keithmaniacop@gmail.com>
2021-01-17 17:12:22 +0800

    Upload endpoints backend and UI actions

--- frontend/src/screens/EditProductScreen.js
+++ frontend/src/screens/EditProductScreen.js
@@ -42,7 +42,7 @@ const ProductEditScreen = ({ match, history }) => {
         dispatch(listProductDetails(productId));
       } else {
         setName(product.name);
-        setPrice(product.email);
+        setPrice(product.price);
         setImage(product.image);
         setBrand(product.brand);
         setCategory(product.category);
