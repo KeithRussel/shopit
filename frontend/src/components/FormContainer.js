840537e80677076c459bd61a851ed077e2c94ef6
KeithRussel <keithmaniacop@gmail.com>
2021-01-22 01:16:54 +0800

    Prepare for deployment

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -3,7 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 
 const FormContainer = ({ children }) => {
   return (
-    <Row className='justify-content-md-center'>
+    <Row className='justify-content-md-center form-margin-auto'>
       <Col xs={12} md={6} className='form-bg'>
         {children}
       </Col>
